081fbe4f931261ff89fe28b8a50cc27a0fbed63c
Mo Refaat <Midorefaat243@gmail.com>
2023-01-09 13:10:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.calculatorapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.calculatorapp.components.InputField
+import com.example.calculatorapp.ui.theme.CalculatorAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                TopHeader()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    CalculatorAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            // modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            content()
+        }
+    }
+}
+
+// @Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 134.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 12.dp)
+            .height(150.dp)
+            .clip(shape = CircleShape.copy(all = CornerSize(20.dp))),
+        color = Color(0xFFE9D7F7)
+    ) {
+        Column(
+            modifier = Modifier.padding(all = 12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+    }
+}
+
+    @Preview
+    @Composable
+    fun MainContent() {
+        Surface(
+            modifier = Modifier
+                .padding(all = 12.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+            border = BorderStroke(width = 1.dp, color = Color.LightGray)
+        ) {
+            Column(modifier = Modifier.padding(all = 5.dp)) {
+
+//                InputField(valueState = ,
+//                    labelId = ,
+//                    enabled = ,
+//                    isSingelLine = )
+            }
+        }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CalculatorAppTheme {
+    }
+}
